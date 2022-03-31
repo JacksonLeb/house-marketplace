5889d9a2f75cb330be81498cae3430b24bf273aa
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-03-30 21:09:10 -0400

    added listings with listings pictures and bed/bath specifications

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,6 +5,7 @@ import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'fir
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem'
 
 function Category() {
 
@@ -65,7 +66,10 @@ function Category() {
             <main>
                 <ul className='categoryListings'>
                     {listings.map((listing) => (
-                        <h3 key={listing.id}>{listing.data.name}</h3>
+                        <ListingItem 
+                        listing={listing.data}
+                        id={listing.id}
+                        />
                     ))}
                 </ul>
             </main>
